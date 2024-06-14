a99a84ac7f2a977e5589464a433eb6e976d2f0ab
Milad Keivanfar <129506375+miladkeivanfar@users.noreply.github.com>
2024-06-14 00:39:32 -0700

    Update heade-scanner.py
    Add Some feature
    Get input from pipe
    Add report
    failure url

--- heade-scanner.py
+++ heade-scanner.py
@@ -1,6 +1,8 @@
 import argparse
 import requests
 import urllib3
+import sys
+import time
 from colorama import Fore, Style
 from tabulate import tabulate
 urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
@@ -26,13 +28,60 @@ class config:
 
     information_headers = ["Server","X-Powered-By","X-AspNet-Version","X-AspNetMvc-Version","Access-Control-Allow-Origin"]
 
+
+def convert_duration(duration):
+    # Convert the duration to seconds
+    total_seconds = duration
+
+    # Calculate hours, minutes, and seconds
+    hours = int(total_seconds // 3600)
+    minutes = int((total_seconds % 3600) // 60)
+    seconds = int(total_seconds % 60)
+
+    # Format the duration
+    if hours > 0:
+        formatted_duration = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+    elif minutes > 0:
+        formatted_duration = f"{minutes:02d}:{seconds:02d}"
+    else:
+        formatted_duration = f"00:00:{seconds:02d}"
+
+    return formatted_duration
+
+def report(all_url,duration_process=None,failure_count=None):
+  print("\n")
+  print(Fore.GREEN + 'Summery Report:\n' + Style.RESET_ALL)
+
+  table = [["Duration", duration_process, "Duration of process"],["","" , ""],["All URLs", all_url, "Number of URL in input file"]
+  ,["Failure URL", failure_count, "Number of Failure URL"]]
+
+  print(tabulate(table, headers=["Name", "Quantity", "Description"],tablefmt="github"))
+
 def check_security_headers(urls, ignore_ssl_errors):
+
+    failure_url = []
+
+    failure_count = 0
 
     for url in urls:
         try:
-            response = requests.get(url, verify=ignore_ssl_errors, headers={"Origin":url})  
+            response = requests.get(url, verify=ignore_ssl_errors, headers={"Origin":url}) 
+
+        except requests.exceptions.SSLError as e:
+        # Increment failure count
+            failure_count += 1
+            failure_url.append(url)
+            pass
+        except requests.exceptions.ConnectionError as e:
+            failure_count += 1
+            failure_url.append(url)
+            pass
         except Exception as e:
+            failure_count += 1
+            failure_url.append(url)
             exit(f"[Error] Failure {url}: {e}")
+            pass
+
 
         headers = response.headers
         status_code = response.status_code
@@ -67,6 +116,18 @@ def check_security_headers(urls, ignore_ssl_errors):
                     table.append([status, header_name, "-"])
 
         print(tabulate(table, headers=["Status", "Header", "Value"]))
+    return failure_count,failure_url
+
+def failure(failure_url):
+    print()
+    table = []
+    subject = Fore.RED + "Failure URLS:" + Style.RESET_ALL
+    for url in failure_url:
+        table.append([url])
+       
+    print(subject,"\n") 
+    print(tabulate(table, headers=["URLs"],tablefmt="github",showindex=range(len(table))))
+    print()
 
 
 def read_urls_from_file(filename):
@@ -87,17 +148,37 @@ def read_urls_from_file(filename):
         return []
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description="Check security headers for URLs")
-    parser.add_argument("-f", "--file", dest="urls_file", required=True,
-                        help="Path to a text file containing URLs, each url in a line (required)")
-    parser.add_argument("--ignore-ssl-errors", action="store_false", default=True,
-                        help="Ignore SSL verification errors ")
+    start = time.time()
+    parser = argparse.ArgumentParser(description="Check security headers for URLs\n")
+    parser.add_argument("-f", "--file", dest="urls_file", required=False, help="Path to a text file containing URLs, each url in a line (required)")
+    parser.add_argument("-r","--report",action="store_true",help="Show a summery report")
+    parser.add_argument("-k","--failure",action="store_true",help="Show failure URL")
+    parser.add_argument("-i","--ignore-ssl-errors", action="store_false", default=True, help="Ignore SSL verification errors ")
     args = parser.parse_args()
 
-    # Read URLs from the file
-    urls = read_urls_from_file(args.urls_file)
+    if not sys.stdin.isatty():
+        urls = [line.strip() for line in sys.stdin.readlines()]
+        all_url = len(urls)
+        if urls:
+            failure_count, failure_url = check_security_headers(urls, ignore_ssl_errors=args.ignore_ssl_errors)
+        else:
+            print("File is empty")
 
-    # Check security headers if URLs were read successfully
-    if urls:
-        check_security_headers(urls, ignore_ssl_errors=args.ignore_ssl_errors)
+    else:
+        # Read URLs from the file
+        urls = read_urls_from_file(args.urls_file)
+        all_url = len(urls)
+
+        # Check security headers if URLs were read successfully
+        if urls:
+            failure_count, failure_url = check_security_headers(urls, ignore_ssl_errors=args.ignore_ssl_errors)
+
+    end = time.time()
+    duration = end - start
+    duration_process = convert_duration(duration)
+
+    if args.failure:
+        failure(failure_url)
+
+    if args.report:
+        report(all_url,duration_process,failure_count)
