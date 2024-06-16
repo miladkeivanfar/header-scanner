054fdeba953fe7232322cdda0477e9cf078d0179
Milad Keivanfar <129506375+miladkeivanfar@users.noreply.github.com>
2024-06-17 01:40:52 +0330

    Update heade_scanner.py

--- heade_scanner.py
+++ heade_scanner.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import argparse
 import requests
 import urllib3
@@ -5,28 +7,30 @@ import sys
 import time
 from colorama import Fore, Style
 from tabulate import tabulate
+
 urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
 
 class config:
     all_headers = ["Content-Security-Policy",
-            "Strict-Transport-Security",
-            "X-Frame-Options",
-            "X-Content-Type-Options",
-            "Cache-Control",
-            "Server",
-            "X-Powered-By",
-            "X-AspNet-Version",
-            "X-AspNetMvc-Version",
-            "Access-Control-Allow-Origin"]
+                   "Strict-Transport-Security",
+                   "X-Frame-Options",
+                   "X-Content-Type-Options",
+                   "Cache-Control",
+                   "Server",
+                   "X-Powered-By",
+                   "X-AspNet-Version",
+                   "X-AspNetMvc-Version",
+                   "Access-Control-Allow-Origin"]
 
     security_headers = [
-            "Content-Security-Policy",
-            "Strict-Transport-Security",
-            "X-Frame-Options",
-            "X-Content-Type-Options",
-            "Cache-Control"]
+        "Content-Security-Policy",
+        "Strict-Transport-Security",
+        "X-Frame-Options",
+        "X-Content-Type-Options",
+        "Cache-Control"]
 
-    information_headers = ["Server","X-Powered-By","X-AspNet-Version","X-AspNetMvc-Version","Access-Control-Allow-Origin"]
+    information_headers = ["Server", "X-Powered-By", "X-AspNet-Version", "X-AspNetMvc-Version",
+                           "Access-Control-Allow-Origin"]
 
 
 def convert_duration(duration):
@@ -48,27 +52,29 @@ def convert_duration(duration):
 
     return formatted_duration
 
-def report(all_url,duration_process=None,failure_count=None):
-  print("\n")
-  print(Fore.GREEN + 'Summery Report:\n' + Style.RESET_ALL)
 
-  table = [["Duration", duration_process, "Duration of process"],["","" , ""],["All URLs", all_url, "Number of URL in input file"]
-  ,["Failure URL", failure_count, "Number of Failure URL"]]
+def report(all_url, duration_process=None, failure_count=None):
+    print("\n")
+    print(Fore.GREEN + 'Summery Report:\n' + Style.RESET_ALL)
 
-  print(tabulate(table, headers=["Name", "Quantity", "Description"],tablefmt="github"))
+    table = [["Duration", duration_process, "Duration of process"], ["", "", ""],
+             ["All URLs", all_url, "Number of URL in input file"]
+        , ["Failure URL", failure_count, "Number of Failure URL"]]
+
+    print(tabulate(table, headers=["Name", "Quantity", "Description"], tablefmt="github"))
+
 
 def check_security_headers(urls, ignore_ssl_errors):
-
     failure_url = []
 
     failure_count = 0
 
     for url in urls:
         try:
-            response = requests.get(url, verify=ignore_ssl_errors, headers={"Origin":url}) 
+            response = requests.get(url, verify=ignore_ssl_errors, headers={"Origin": url})
 
         except requests.exceptions.SSLError as e:
-        # Increment failure count
+            # Increment failure count
             failure_count += 1
             failure_url.append(url)
             pass
@@ -82,21 +88,20 @@ def check_security_headers(urls, ignore_ssl_errors):
             exit(f"[Error] Failure {url}: {e}")
             pass
 
-
         headers = response.headers
         status_code = response.status_code
 
-        if status_code in range (200, 299):
-            print(f"\n{Fore.CYAN + url + Style.RESET_ALL}" , " ", Fore.GREEN + str(status_code) + Style.RESET_ALL)
+        if status_code in range(200, 299):
+            print(f"\n{Fore.CYAN + url + Style.RESET_ALL}", " ", Fore.GREEN + str(status_code) + Style.RESET_ALL)
 
-        elif status_code in range (300, 399):
-            print(f"\n{Fore.CYAN + url + Style.RESET_ALL}" , " ", Fore.BLUE + str(status_code) + Style.RESET_ALL)
+        elif status_code in range(300, 399):
+            print(f"\n{Fore.CYAN + url + Style.RESET_ALL}", " ", Fore.BLUE + str(status_code) + Style.RESET_ALL)
 
-        elif status_code in range (400, 499):
-            print(f"\n{Fore.CYAN + url + Style.RESET_ALL}" , " ", Fore.RED + str(status_code) + Style.RESET_ALL)
+        elif status_code in range(400, 499):
+            print(f"\n{Fore.CYAN + url + Style.RESET_ALL}", " ", Fore.RED + str(status_code) + Style.RESET_ALL)
 
-        elif status_code in range (500, 599):
-            print(f"\n{Fore.CYAN + url + Style.RESET_ALL}" , " ", Fore.RED + str(status_code) + Style.RESET_ALL)
+        elif status_code in range(500, 599):
+            print(f"\n{Fore.CYAN + url + Style.RESET_ALL}", " ", Fore.RED + str(status_code) + Style.RESET_ALL)
 
         # Check for each security header
         table = []
@@ -104,19 +109,20 @@ def check_security_headers(urls, ignore_ssl_errors):
             if header_name in headers:
 
                 if header_name in config.security_headers:
-                    status = Fore.GREEN + "Match" + Style.RESET_ALL 
+                    status = Fore.GREEN + "Match" + Style.RESET_ALL
                     table.append([status, header_name, headers[header_name]])
 
                 if header_name in config.information_headers:
                     status = Fore.BLUE + "Information" + Style.RESET_ALL
                     table.append([status, header_name, headers[header_name]])
 
-            elif header_name in config.security_headers: 
-                    status = Fore.YELLOW + "Warning" + Style.RESET_ALL  
-                    table.append([status, header_name, "-"])
+            elif header_name in config.security_headers:
+                status = Fore.YELLOW + "Warning" + Style.RESET_ALL
+                table.append([status, header_name, "-"])
 
         print(tabulate(table, headers=["Status", "Header", "Value"]))
-    return failure_count,failure_url
+    return failure_count, failure_url
+
 
 def failure(failure_url):
     print()
@@ -124,14 +130,13 @@ def failure(failure_url):
     subject = Fore.RED + "Failure URLS:" + Style.RESET_ALL
     for url in failure_url:
         table.append([url])
-       
-    print(subject,"\n") 
-    print(tabulate(table, headers=["URLs"],tablefmt="github",showindex=range(len(table))))
+
+    print(subject, "\n")
+    print(tabulate(table, headers=["URLs"], tablefmt="github", showindex=range(len(table))))
     print()
 
 
 def read_urls_from_file(filename):
-
     if filename is None:
         print("Error: No file path provided.")
         return []  # Return an empty list if no filename is given
@@ -147,13 +152,16 @@ def read_urls_from_file(filename):
         print(f"Error: Permission denied to read file: {filename}")
         return []
 
+
 if __name__ == "__main__":
     start = time.time()
     parser = argparse.ArgumentParser(description="Check security headers for URLs\n")
-    parser.add_argument("-f", "--file", dest="urls_file", required=False, help="Path to a text file containing URLs, each url in a line (required)")
-    parser.add_argument("-r","--report",action="store_true",help="Show a summery report")
-    parser.add_argument("-k","--failure",action="store_true",help="Show failure URL")
-    parser.add_argument("-i","--ignore-ssl-errors", action="store_false", default=True, help="Ignore SSL verification errors ")
+    parser.add_argument("-f", "--file", dest="urls_file", required=False,
+                        help="Path to a text file containing URLs, each url in a line (required)")
+    parser.add_argument("-r", "--report", action="store_true", help="Show a summery report")
+    parser.add_argument("-k", "--failure", action="store_true", help="Show failure URL")
+    parser.add_argument("-i", "--ignore-ssl-errors", action="store_false", default=True,
+                        help="Ignore SSL verification errors ")
     args = parser.parse_args()
 
     if not sys.stdin.isatty():
@@ -165,13 +173,16 @@ if __name__ == "__main__":
             print("File is empty")
 
     else:
-        # Read URLs from the file
-        urls = read_urls_from_file(args.urls_file)
-        all_url = len(urls)
+        if args.urls_file:
+            # Read URLs from the file
+            urls = read_urls_from_file(args.urls_file)
+            all_url = len(urls)
 
-        # Check security headers if URLs were read successfully
-        if urls:
-            failure_count, failure_url = check_security_headers(urls, ignore_ssl_errors=args.ignore_ssl_errors)
+            # Check security headers if URLs were read successfully
+            if urls:
+                failure_count, failure_url = check_security_headers(urls, ignore_ssl_errors=args.ignore_ssl_errors)
+        else:
+            print("Please enter your urls file. Example -f urls.txt")
 
     end = time.time()
     duration = end - start
@@ -181,4 +192,4 @@ if __name__ == "__main__":
         failure(failure_url)
 
     if args.report:
-        report(all_url,duration_process,failure_count)
+        report(all_url, duration_process, failure_count)
